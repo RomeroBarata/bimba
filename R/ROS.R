c5aa07cccf58d64acc945f61a1f761dbe1d088b9
RomeroBarata <romero.barata@gmail.com>
2018-01-05 13:03:28 -0300

    Implement the Random Over-Sampling (ROS) and Random Under-Sampling (RUS) algorithms.

---
+++ R/ROS.R
@@ -0,0 +1,59 @@
+#' The Random Over-Sampling algorithm.
+#'
+#' \code{ROS} returns a more balanced version of a data set after application
+#' of the Random Over-Sampling algorithm.
+#'
+#' The Random Over-Sampling algorithm works by appending randomly selected
+#' examples from the minority class (with replacement) to the original data
+#' set.
+#'
+#' @param data A data frame containing the predictors and the outcome. The
+#'  outcome must be both a binary valued factor and the last column of
+#'  \code{data}.
+#' @param perc_min The desired \% size of the minority class relative to the
+#'  whole data set. For instance, if \code{perc_min} = 50 the returned data
+#'  set is balanced. \code{perc_min} is ignored if \code{perc_over} is
+#'  specified.
+#' @param perc_over \% of examples to append to the input data set relative
+#'  to the size of the minority class. For instance, if \code{perc_over} = 100
+#'  the minority class doubles in size. If specified, \code{perc_min} is
+#'  ignored.
+#' @param classes A named vector identifying the majority and the minority
+#'  classes. The names must be "Majority" and "Minority". This argument is
+#'  only useful if the function is called inside another sampling function.
+#' @return A data frame containing a more balanced version of the input data
+#'  set after application of the Random Over-Sampling algorithm.
+#' @examples
+#' imb_data <- generate_imbalanced_data(num_examples = 200, 
+#'                                      num_features = 2,
+#'                                      imbalance_ratio = 5,
+#'                                      noise_maj = 0,
+#'                                      noise_min = 0,
+#'                                      seed = 42)
+#'  
+#' table(imb_data$target)
+#' table(ROS(imb_data, perc_min = 50)$target)    # Balance the classes
+#' table(ROS(imb_data, perc_over = 100)$target)  # Double minority class size
+#' @export
+
+ROS <- function(data, perc_min = 50, perc_over = NULL, classes = NULL){
+  y <- data[[ncol(data)]]
+  if (is.null(classes)) classes <- extract_classes(y)
+
+  min_idx <- which(y == classes[["Minority"]])
+
+  maj_size <- length(which(y == classes[["Majority"]]))
+  min_size <- length(min_idx)
+
+  sample_size <- compute_oversample_size(majority_size = maj_size,
+                                         minority_size = min_size,
+                                         perc_min = perc_min,
+                                         perc_over = perc_over)
+  if (sample_size == 0) return(data)
+
+  # Avoid for instance sample(22, size = 30, replace = TRUE)
+  if (min_size == 1) min_idx <- rep(min_idx, 2)
+  append_idx <- sample(min_idx, size = sample_size, replace = TRUE)
+
+  rbind(data, data[append_idx, , drop = FALSE])
+}
---
+++ R/RUS.R
@@ -0,0 +1,51 @@
+#' The Random Under-Sampling algorithm.
+#'
+#' \code{RUS} returns a more balanced version of a data set after application
+#' of the Random Under-Sampling algorithm.
+#'
+#' The Random Under-Sampling algorithm creates a new data set containing all 
+#' examples from the minority class plus a random selection of examples from 
+#' the majority class.
+#' 
+#' @inheritParams ROS
+#' @param perc_maj The desired \% size of the majority class relative to the
+#'  whole data set. For instance, if \code{perc_maj} = 50 a balanced version
+#'  of the input data set is returned. \code{perc_maj} is ignored if
+#'  \code{perc_under} is specified.
+#' @param perc_under \% of examples to select from the majority class. If
+#'  specified \code{perc_maj} is ignored.
+#' @return A data frame containing a more balanced version of the input data
+#'  set after application of the Random Under-Sampling algorithm. The original
+#'  order of the examples is preserved.
+#' @examples
+#' imb_data <- generate_imbalanced_data(num_examples = 200, 
+#'                                      num_features = 2,
+#'                                      imbalance_ratio = 5,
+#'                                      noise_maj = 0,
+#'                                      noise_min = 0,
+#'                                      seed = 42)
+#'  
+#' table(imb_data$target)
+#' table(RUS(imb_data, perc_maj = 50)$target)    # Balance the classes
+#' table(RUS(imb_data, perc_under = 20)$target)  # Select 20% of maj. class
+#' @export
+
+RUS <- function(data, perc_maj = 50, perc_under = NULL, classes = NULL){
+  y <- data[[ncol(data)]]
+  if (is.null(classes)) classes <- extract_classes(y)
+
+  maj_idx <- which(y == classes[["Majority"]])
+
+  maj_size <- length(maj_idx)
+  min_size <- sum(y == classes[["Minority"]])
+
+  sample_size <- compute_undersample_size(majority_size = maj_size,
+                                          minority_size = min_size,
+                                          perc_maj = perc_maj,
+                                          perc_under = perc_under)
+  if (sample_size == 0) return(data)
+
+  maj_idx_remove <- sample(maj_idx, size = sample_size, replace = FALSE)
+
+  data[-maj_idx_remove, , drop = FALSE]
+}
